e20e527b0a44bf3ef3d836f06ab7e99d319c5bb0
yamazaki <t-yamazaki@yasuda-seiki.co.jp>
2020-12-24 14:00:54 +0900

    作業記録

---
+++ OOPPractice/Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Stock
+    {
+        private int quantity;
+
+        public Stock(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+        }
+
+        public void Decrement()
+        {
+            this.quantity--;
+        }
+    }
+}
