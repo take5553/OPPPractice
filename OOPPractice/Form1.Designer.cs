dff935c134b79ba33d5c47267d6fe39bcd8eaeed
Takeshi <yamazakitakeshi02@gmail.com>
2021-01-01 18:40:02 +0900

    VendingMachineクラスのクライアントを調整

--- OOPPractice/Form1.Designer.cs
+++ OOPPractice/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(258, 166);
+            this.button1.Location = new System.Drawing.Point(85, 93);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(231, 73);
             this.button1.TabIndex = 0;
@@ -41,21 +44,56 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- OOPPractice/Form1.cs
+++ OOPPractice/Form1.cs
@@ -12,6 +12,8 @@ namespace OOPPractice
 {
     public partial class Form1 : Form
     {
+        private VendingMachine vm = new VendingMachine();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,18 +21,33 @@ namespace OOPPractice
 
         private void button1_Click(object sender, EventArgs e)
         {
-            VendingMachine vm = new VendingMachine();
 
+            Drink drink = vm.Buy(100, Drink.COKE);
+            int charge = vm.Refund();
+
+            if (drink != null && drink.getKind() == Drink.COKE)
+            {
+                label1.Text = "コーラを購入しました。\r";
+                label1.Text = label1.Text + "おつりは" + charge + "円です。";
+            } else
+            {
+                label1.Text = "コーラ買えんかった(´ﾟдﾟ｀)";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             Drink drink = vm.Buy(500, Drink.COKE);
             int charge = vm.Refund();
 
             if (drink != null && drink.getKind() == Drink.COKE)
             {
-                Console.WriteLine("コーラを購入しました。");
-                Console.WriteLine("おつりは" + charge + "円です。");
-            } else
+                label2.Text = "コーラを購入しました。\r";
+                label2.Text = label1.Text + "おつりは" + charge + "円です。";
+            }
+            else
             {
-                Console.WriteLine("コーラ買えんかった(´ﾟдﾟ｀)");
+                label2.Text = "コーラ買えんかった(´ﾟдﾟ｀)";
             }
         }
     }
